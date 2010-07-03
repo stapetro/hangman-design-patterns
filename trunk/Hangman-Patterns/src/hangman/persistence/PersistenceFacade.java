19fd3eb3c6487cccb8fce8fad1637349e9acf33b
kbailov <kbailov@47e568b5-045d-55d2-85c0-dd34ee1906ef>
2010-07-03 20:44:41 +0000

    Implement classes related to internationalization

---
+++ trunk/Hangman-Patterns/src/hangman/persistence/PersistenceFacade.java
@@ -0,0 +1,53 @@
+package hangman.persistence;
+
+import hangman.domain.config.ConfigurationItem;
+import hangman.domain.config.LanguageConfigurationItem;
+
+import java.util.List;
+
+//TODO methods should be implemented
+public class PersistenceFacade implements IPersistenceFacade{
+
+	@Override
+	public void addCategory(ConfigurationItem categoryItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addLanguage(LanguageConfigurationItem languageItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ConfigurationItem> getCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LanguageConfigurationItem getCurrentLanguage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<LanguageConfigurationItem> getLanguages() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeCategory(int categoryId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeLanguage(int languageId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
