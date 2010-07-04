04abd197826bb36019b76eec1ae786bae308dd37
kbailov <kbailov@47e568b5-045d-55d2-85c0-dd34ee1906ef>
2010-07-04 17:34:27 +0000

    Add Menu Items and update Internationalization files

--- trunk/Hangman-Patterns/src/hangman/presentation/ConsolePanel.java
+++ trunk/Hangman-Patterns/src/hangman/presentation/ConsolePanel.java
@@ -2,10 +2,13 @@ package hangman.presentation;
 
 import java.awt.GridBagLayout;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.GridBagConstraints;
 
 public class ConsolePanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JTextArea consoleTextArea = null;
 
 	/**
 	 * This is the default constructor
@@ -21,8 +24,27 @@ public class ConsolePanel extends JPanel {
 	 * @return void
 	 */
 	private void initialize() {
-		this.setSize(300, 200);
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.fill = GridBagConstraints.BOTH;
+		gridBagConstraints.gridy = 0;
+		gridBagConstraints.weightx = 1.0;
+		gridBagConstraints.weighty = 1.0;
+		gridBagConstraints.gridx = 0;
+		this.setSize(203, 258);
 		this.setLayout(new GridBagLayout());
+		this.add(getConsoleTextArea(), gridBagConstraints);
+	}
+
+	/**
+	 * This method initializes consoleTextArea	
+	 * 	
+	 * @return javax.swing.JTextArea	
+	 */
+	private JTextArea getConsoleTextArea() {
+		if (consoleTextArea == null) {
+			consoleTextArea = new JTextArea();
+		}
+		return consoleTextArea;
 	}
 
 }
