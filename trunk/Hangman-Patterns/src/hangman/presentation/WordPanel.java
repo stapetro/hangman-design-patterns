4ef12e8d7223e14e330912533c9c1e432c2316d3
kbailov <kbailov@47e568b5-045d-55d2-85c0-dd34ee1906ef>
2010-07-04 22:07:46 +0000

    Implement handlers for letter button clicks

--- trunk/Hangman-Patterns/src/hangman/presentation/WordPanel.java
+++ trunk/Hangman-Patterns/src/hangman/presentation/WordPanel.java
@@ -4,20 +4,25 @@ import hangman.languages.LanguageResourcesFactory;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ResourceBundle;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class WordPanel extends JPanel {
-	
+
 	private static final String ALPHABET_STR = "alphabet";
 	private static final String COMMA = ",";
 
-	private ResourceBundle resourceBundle;  //  @jve:decl-index=0:
+	private ResourceBundle resourceBundle; // @jve:decl-index=0:
 
 	private static final long serialVersionUID = 1L;
 	private JPanel alphabetPanel = null;
+	private JLabel secretWordLabel = null;
 
 	/**
 	 * This is the default constructor
@@ -35,18 +40,24 @@ public class WordPanel extends JPanel {
 	private void initialize() {
 		resourceBundle = LanguageResourcesFactory.getLanguageResource();
 
+		secretWordLabel = new JLabel();
+		//TODO should take the word from the game
+		secretWordLabel.setText("SECRET WORD _ _ _ ");
 		this.setSize(300, 200);
 		this.setLayout(new BorderLayout());
 		this.add(getAlphabetPanel(), BorderLayout.SOUTH);
+		this.add(secretWordLabel, BorderLayout.CENTER);
 		populateAlphabetLetters(alphabetPanel);
 	}
-	
-	private void populateAlphabetLetters(JPanel alphabetPnl){
+
+	private void populateAlphabetLetters(JPanel alphabetPnl) {
 		String alphabet = resourceBundle.getString(ALPHABET_STR);
 		String[] alphabetChars = alphabet.split(COMMA);
-		
-		for(int i = 0; i < alphabetChars.length; i++){
+
+		for (int i = 0; i < alphabetChars.length; i++) {
 			JButton letterBtn = new JButton(alphabetChars[i]);
+			LetterButtonHangler letterBtnHangler = new LetterButtonHangler();
+			letterBtn.addActionListener(letterBtnHangler);
 			alphabetPnl.add(letterBtn);
 		}
 	}
@@ -64,4 +75,16 @@ public class WordPanel extends JPanel {
 		return alphabetPanel;
 	}
 
+}
+
+/**
+ * class handlink button clicks.
+ * 
+ */
+class LetterButtonHangler implements ActionListener {
+	public void actionPerformed(ActionEvent event) {
+		// TODO the below is for test purpose. Should be implemented properly
+		// later
+		JOptionPane.showMessageDialog(null, event.getActionCommand());
+	}
 }
