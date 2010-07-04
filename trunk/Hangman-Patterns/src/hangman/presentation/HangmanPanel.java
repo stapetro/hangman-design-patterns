40e1fb1d12fbcc1c2e334076782fcec8ce716140
kbailov <kbailov@47e568b5-045d-55d2-85c0-dd34ee1906ef>
2010-07-04 16:58:49 +0000

    Create some of the UI Panels

---
+++ trunk/Hangman-Patterns/src/hangman/presentation/HangmanPanel.java
@@ -0,0 +1,28 @@
+package hangman.presentation;
+
+import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+
+public class HangmanPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * This is the default constructor
+	 */
+	public HangmanPanel() {
+		super();
+		initialize();
+	}
+
+	/**
+	 * This method initializes this
+	 * 
+	 * @return void
+	 */
+	private void initialize() {
+		this.setSize(300, 200);
+		this.setLayout(new GridBagLayout());
+	}
+
+}
