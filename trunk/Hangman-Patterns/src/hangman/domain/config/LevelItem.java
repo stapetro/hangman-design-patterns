7b499d83bef1f86d6faa5030a6544f4a1d028366
devilfighter1806 <devilfighter1806@47e568b5-045d-55d2-85c0-dd34ee1906ef>
2010-07-06 19:06:20 +0000

    Console panel logging improved. Gets random word appropriate for the current level

--- trunk/Hangman-Patterns/src/hangman/domain/config/LevelItem.java
+++ trunk/Hangman-Patterns/src/hangman/domain/config/LevelItem.java
@@ -7,23 +7,26 @@ import java.util.HashMap;
 public class LevelItem extends ConfigurationItem {
 
 	private String name;
-	private int wordLength;
+	private int minWordLength;
+	private int maxWordLength;
 	private int totalMistakes;
 
 	public LevelItem() {
 		super();
 	}
 
-	public LevelItem(int id, String desc, String name, int wordLength,
-			int totalMistakes) {
+	public LevelItem(int id, String desc, String name, int minWordLength,
+			int maxWordLength, int totalMistakes) {
 		super(id, desc);
 		this.name = name;
-		this.wordLength = wordLength;
+		this.minWordLength = minWordLength;
+		this.maxWordLength = maxWordLength;
 		this.totalMistakes = totalMistakes;
 	}
 
-	public LevelItem(int id, String name, int wordLength, int totalMistakes) {
-		this(id, "", name, wordLength, totalMistakes);
+	public LevelItem(int id, String name, int wordLength, int maxWordLength,
+			int totalMistakes) {
+		this(id, "", name, wordLength, maxWordLength, totalMistakes);
 	}
 
 	public LevelItem(HashMap<String, String> levelProperties) {
@@ -47,12 +50,20 @@ public class LevelItem extends ConfigurationItem {
 		this.name = name;
 	}
 
-	public int getWordLength() {
-		return wordLength;
+	public int getMinWordLength() {
+		return minWordLength;
 	}
 
-	public void setWordLength(int wordLength) {
-		this.wordLength = wordLength;
+	public void setMinWordLength(int wordLength) {
+		this.minWordLength = wordLength;
+	}
+
+	public int getMaxWordLength() {
+		return maxWordLength;
+	}
+
+	public void setMaxWordLength(int maxWordLength) {
+		this.maxWordLength = maxWordLength;
 	}
 
 	public int getTotalMistakes() {
@@ -67,8 +78,9 @@ public class LevelItem extends ConfigurationItem {
 	public String toString() {
 		return String
 				.format(
-						"Id: %d, Desc: %s, Name: %s, Word length: %d, Total mistakes: %s%n",
-						getId(), getDescription(), this.name, this.wordLength,
+						"Id: %d, Desc: %s, Name: %s, Word length: [%d, %d], Total mistakes: %s%n",
+						getId(), getDescription(), this.name,
+						this.minWordLength, this.maxWordLength,
 						this.totalMistakes);
 	}
 
@@ -90,11 +102,15 @@ public class LevelItem extends ConfigurationItem {
 			this.name = levelPropValue;
 			break;
 		case WORD_LENGTH:
-			try {
-				this.wordLength = Integer.parseInt(levelPropValue);
-			} catch (NumberFormatException ex) {
-				System.out.println(errorMsg);
-				ex.printStackTrace();
+			String[] wordSizes = levelPropValue.split(",");
+			if (wordSizes.length > 0) {
+				try {
+					this.minWordLength = Integer.parseInt(wordSizes[0]);
+					this.maxWordLength = Integer.parseInt(wordSizes[1]);
+				} catch (NumberFormatException ex) {
+					System.out.println(errorMsg);
+					ex.printStackTrace();
+				}
 			}
 			break;
 		case TOTAL_MISTAKES:
