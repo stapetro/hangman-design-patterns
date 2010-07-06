0faeeefa161acc7839362e16a7acfa97d228529c
devilfighter1806 <devilfighter1806@47e568b5-045d-55d2-85c0-dd34ee1906ef>
2010-07-06 21:48:46 +0000

    Hangman frame

--- Hangman-Patterns/src/hangman/presentation/HangmanPanel.java
+++ Hangman-Patterns/src/hangman/presentation/HangmanPanel.java
@@ -1,9 +1,14 @@
 package hangman.presentation;
 
+import hangman.logic.WordMask;
+
 import java.awt.GridBagLayout;
+import java.util.Observable;
+import java.util.Observer;
+
 import javax.swing.JPanel;
 
-public class HangmanPanel extends JPanel {
+public class HangmanPanel extends JPanel implements Observer {
 
 	private static final long serialVersionUID = 1L;
 	/**
@@ -22,6 +27,23 @@ public class HangmanPanel extends JPanel {
 	private void initialize() {
 		this.setSize(300, 200);
 		this.setLayout(new GridBagLayout());
+	}
+
+	@Override
+	public void update(Observable wordMaskObservable, Object arg) {
+		System.out.println("test....");
+		if(wordMaskObservable instanceof WordMask) {
+			WordMask wordMask = (WordMask)wordMaskObservable;
+			if(wordMask != null) {
+				if(wordMask.isHung()) {
+					System.out.println("hug....");
+				} else {
+					int currentTotalMistakes = wordMask.getTotalMistakes();
+					int maxMisaktes = wordMask.getMaxNumberOfMistakes();
+					System.out.println(currentTotalMistakes + "/" + maxMisaktes);
+				}
+			}
+		}
 	}
 
 }
