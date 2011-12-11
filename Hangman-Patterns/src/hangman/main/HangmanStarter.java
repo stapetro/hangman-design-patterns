176c8bab75f01eb76702407135958f315e2fddf9
devilfighter1806 <devilfighter1806@47e568b5-045d-55d2-85c0-dd34ee1906ef>
2011-12-11 18:48:16 +0000

    Main method starts presentation part

--- Hangman-Patterns/src/hangman/main/HangmanStarter.java
+++ Hangman-Patterns/src/hangman/main/HangmanStarter.java
@@ -1,36 +1,26 @@
 package hangman.main;
 
-import hangman.domain.config.LanguageItem;
-import hangman.domain.config.LevelItem;
-import hangman.languages.LanguageResourcesFactory;
-import hangman.persistence.IPersistenceFacade;
-import hangman.persistence.PersistenceFacade;
+import hangman.presentation.HangmanFrame;
 
-import java.util.List;
-import java.util.ResourceBundle;
+import javax.swing.JFrame;
 
 public class HangmanStarter {
+	
+	public HangmanStarter() {
+		HangmanFrame frame = new HangmanFrame();
+		//Optional: What happens when the frame closes?
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Size the frame.
+		frame.pack();
+		//Show it.
+		frame.setVisible(true);
+	}
+	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		IPersistenceFacade facade = new PersistenceFacade();
-//		List<LanguageItem> langs = facade.getLanguages();
-//		if (langs != null) {
-//			System.out.println(langs.toString());
-//		}
-//		System.out.println("Current lang:\n" + facade.getCurrentLanguage());
-//		System.out.println("Current score board size: "
-//				+ facade.getCurrentScoreBoardSize());
-//		List<LevelItem> levels = facade.getLevels();
-//		if (levels != null) {
-//			System.out.println(levels.toString());
-//		}
-//		System.out.println(facade.getCurrentLevel());
-//		
-//		ResourceBundle languageResource = LanguageResourcesFactory.getLanguageResource();
-//		System.out.println(languageResource.getString("language"));
+		new HangmanStarter();
 	}
-
 }
